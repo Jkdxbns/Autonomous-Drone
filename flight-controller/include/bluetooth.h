143acd2c2c039f8d086367f11bf2ead70037a82d
Justin Mascarenhas <jmascarenhas1998@gmail.com>
2025-11-13 15:37:00 -0500

    Drone: motor controlling using bluetooth

--- flight-controller/include/bluetooth.h
+++ flight-controller/include/bluetooth.h
@@ -8,9 +8,10 @@
 /**
  * Parse and handle a single Bluetooth command line.
  * Supported commands:
- *  - 0..100 : set throttle percentage
- *  - 1111   : decrease ESC_MAX_US by 100us (clamped)
- *  - 9999   : increase ESC_MAX_US by 100us (clamped)
+ *  - throttle:0..100 : set throttle percentage
+ *  - limit_decrease  : decrease ESC_MAX_US by 100us (clamped)
+ *  - limit_increase  : increase ESC_MAX_US by 100us (clamped)
+ *  - e-stop          : emergency stop - immediately set all motors to 0%
  *
  * @param command null-terminated ASCII string (modified only for reading)
  */
